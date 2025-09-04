2d898640f1d0f2eddb839c7032243070492dab95
Eason <admin@56fkj.cn>
2025-09-04 15:42:16 +0800

    Update natives.cpp

--- src/natives.cpp
+++ src/natives.cpp
@@ -6,6 +6,7 @@
 #include "natives.h"
 
 #include "MqttClientMgr.h"
+#include "CallbackDispatcher.h"
 
 cell AMX_NATIVE_CALL mqtt_create(AMX *amx, cell *params) {
     enum { arg_count, arg_blocker, arg_client_id };
@@ -17,8 +18,6 @@ cell AMX_NATIVE_CALL mqtt_create(AMX *amx, cell *params) {
 
     const std::string blocker{MF_GetAmxString(amx, params[arg_blocker], arg_blocker - 1, nullptr)};
     const std::string clientId{MF_GetAmxString(amx, params[arg_client_id], arg_client_id - 1, nullptr)};
-
-    MF_Log("%s|%s", blocker.c_str(), clientId.c_str());
 
     return g_mqttClientMgr.make(blocker, clientId);
 }
@@ -43,11 +42,25 @@ cell AMX_NATIVE_CALL mqtt_connect(AMX *amx, cell *params) {
         return 0;
     }
 
-    const MqttClient *client = g_mqttClientMgr.getClient(params[arg_handle]);
+    const AmxxMqttClient *client = g_mqttClientMgr.getClient(params[arg_handle]);
     if (client == nullptr) {
         MF_LogError(amx, AMX_ERR_NATIVE, "Invalid mqtt client handle: %d", params[arg_handle]);
         return 0;
     }
+
+    client->setConnectedHandler([&client](const mqtt::string &) {
+        ConnectedCallbackDispatcher(client);
+    });
+    client->setMessageHandler([&client](const mqtt::const_message_ptr &msg) {
+        MessageCallbackDispatcher(client, msg);
+    });
+    client->setConnectioLostHandler([&client](const mqtt::string &msg) {
+        ConnectionLostCallbackDispatcher(client, msg);
+    });
+    client->setDisconnectHandler([&client](const mqtt::properties &, const mqtt::ReasonCode code) {
+        DisconnectedCallbackDispatcher(client, code);
+    });
+
     client->connect();
     return 1;
 }
@@ -60,7 +73,7 @@ cell AMX_NATIVE_CALL mqtt_is_connect(AMX *amx, cell *params) {
         return 0;
     }
 
-    const MqttClient *client = g_mqttClientMgr.getClient(params[arg_handle]);
+    const AmxxMqttClient *client = g_mqttClientMgr.getClient(params[arg_handle]);
     if (client == nullptr) {
         MF_LogError(amx, AMX_ERR_NATIVE, "Invalid mqtt client handle: %d", params[arg_handle]);
         return 0;
@@ -68,7 +81,7 @@ cell AMX_NATIVE_CALL mqtt_is_connect(AMX *amx, cell *params) {
     return client->isConnected();
 }
 
-cell AMX_NATIVE_CALL mqtt_set_msg_callback(AMX *amx, cell *params) {
+cell AMX_NATIVE_CALL mqtt_set_message_callback(AMX *amx, cell *params) {
     enum { arg_count, arg_handle, arg_handler };
 
     if (params[arg_count] < 2) {
@@ -76,21 +89,23 @@ cell AMX_NATIVE_CALL mqtt_set_msg_callback(AMX *amx, cell *params) {
         return 0;
     }
 
-    const MqttClient *client = g_mqttClientMgr.getClient(params[arg_handle]);
+    AmxxMqttClient *client = g_mqttClientMgr.getClient(params[arg_handle]);
     if (client == nullptr) {
         MF_LogError(amx, AMX_ERR_NATIVE, "Invalid mqtt client handle: %d", params[arg_handle]);
         return 0;
     }
 
-    client->setMessageHandler([&] (const mqtt::const_message_ptr& msg) {
-        client->connect();
-    });
-
     const std::string handler{MF_GetAmxString(amx, params[arg_handler], arg_handler - 1, nullptr)};
 
+    const int forwardId = MF_RegisterSPForwardByName(amx, handler.c_str());
+    if (forwardId != -1) {
+        MF_LogError(amx, AMX_ERR_NATIVE, "Callback function \"%s\" is not exists", handler.c_str());
+        return 0;
+    }
+
+    client->setOnMessageForwardId(forwardId);
     return 1;
 }
-
 
 AMX_NATIVE_INFO g_natives[] =
 {
@@ -98,5 +113,6 @@ AMX_NATIVE_INFO g_natives[] =
     {"mqtt_destroy", mqtt_destroy},
     {"mqtt_connect", mqtt_connect},
     {"mqtt_is_connect", mqtt_is_connect},
+    {"mqtt_set_message_callback", mqtt_set_message_callback},
     {nullptr, nullptr}
 };
