91d4ce897fbf95a67e6a0142311d5ff7188782b9
Eason <admin@56fkj.cn>
2025-09-04 12:15:57 +0800

    mqtt客户端管理器

--- src/MqttClientMgr.cpp
+++ src/MqttClientMgr.cpp
@@ -5,4 +5,37 @@
 #include "MqttClientMgr.h"
 
 MqttClientMgr::~MqttClientMgr() {
+    for (auto &[k, v]: this->m_clients) {
+        delete v;
+    }
 }
+
+int MqttClientMgr::make(const std::string &blocker, const std::string &clientId) {
+    if (const int index = this->makeIndex(); index != 0) {
+        this->m_clients[index] = new MqttClient{blocker, clientId};
+        return index;
+    }
+    return 0;
+}
+
+void MqttClientMgr::destroy(const int index) {
+    if (const auto it = this->m_clients.find(index); it != m_clients.end()) {
+        delete it->second;
+        this->m_indexUsed.erase(it->first);
+        this->m_clients.erase(it);
+    }
+}
+
+int MqttClientMgr::makeIndex() {
+    std::lock_guard lock(this->m_mutex);
+    for (int i = 1; i <= MAX_INDEX; i++) {
+        if (this->m_indexUsed.find(i) == this->m_indexUsed.end()) {
+            this->m_indexUsed.insert(i);
+            return i;
+        }
+    }
+
+    return 0;
+}
+
+MqttClientMgr g_mqttClientMgr;
--- src/MqttClientMgr.h
+++ src/MqttClientMgr.h
@@ -6,15 +6,41 @@
 #define MQTT_CLIENT_MGR_H
 
 #include <unordered_map>
+#include <mutex>
+#include <set>
 
 #include "MqttClient.h"
 
-class MqttClientMgr {
-    std::unordered_map<int, MqttClient*> clients;
-public:
-    MqttClientMgr() = delete;
+#define MAX_INDEX 10000
 
+class MqttClientMgr {
+    std::unordered_map<int, MqttClient *> m_clients;
+    std::set<int> m_indexUsed;
+    std::mutex m_mutex;
+
+public:
     ~MqttClientMgr();
+
+    /**
+     * Make a mqtt client
+     *
+     * @param blocker block server uri
+     * @param clientId client id
+     *
+     * @return index for mqtt client
+     */
+    int make(const std::string &blocker, const std::string &clientId);
+
+    void destroy(int index);
+
+private:
+    /**
+     * make an index for mqtt client on making mqtt client
+     * @return
+     */
+    int makeIndex();
 };
+
+extern MqttClientMgr g_mqttClientMgr;
 
 #endif
