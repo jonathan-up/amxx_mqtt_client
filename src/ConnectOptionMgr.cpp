119e0d0b9969de04625f021c448643ea4da758c4
Eason <admin@56fkj.cn>
2025-09-08 15:52:00 +0800

    Create ConnectOptionMgr

---
+++ src/ConnectOptionMgr.cpp
@@ -0,0 +1,54 @@
+//
+// Created by jonathan on 9/2/25.
+//
+
+#include "ConnectOptionMgr.h"
+
+ConnectOptionMgr::~ConnectOptionMgr() {
+    for (auto &[k, v]: this->m_builder) {
+        delete v;
+    }
+}
+
+int ConnectOptionMgr::make() {
+    if (const int index = this->makeIndex(); index != 0) {
+        this->m_builder[index] = new mqtt::connect_options_builder{};
+        return index;
+    }
+    return 0;
+}
+
+mqtt::connect_options_builder *ConnectOptionMgr::getOption(const int index) {
+    return this->m_builder[index];
+}
+
+void ConnectOptionMgr::destroy(const int index) {
+    if (const auto it = this->m_builder.find(index); it != m_builder.end()) {
+        delete it->second;
+        this->m_indexUsed.erase(it->first);
+        this->m_builder.erase(it);
+    }
+}
+
+void ConnectOptionMgr::reset() {
+    for (auto &[k, v]: this->m_builder) {
+        delete v;
+    }
+
+    this->m_indexUsed.clear();
+    this->m_builder.clear();
+}
+
+int ConnectOptionMgr::makeIndex() {
+    std::lock_guard lock(this->m_mutex);
+    for (int i = 1; i <= MAX_INDEX; i++) {
+        if (this->m_indexUsed.find(i) == this->m_indexUsed.end()) {
+            this->m_indexUsed.insert(i);
+            return i;
+        }
+    }
+
+    return 0;
+}
+
+ConnectOptionMgr g_connectOptionMgr;
---
+++ src/ConnectOptionMgr.h
@@ -0,0 +1,38 @@
+//
+// Created by jonathan on 9/2/25.
+//
+
+#ifndef CONNECT_OPTION_H
+#define CONNECT_OPTION_H
+
+#include <unordered_map>
+#include <mutex>
+#include <set>
+
+#include <mqtt/connect_options.h>
+
+#define MAX_INDEX 10000
+
+class ConnectOptionMgr {
+    std::unordered_map<int, mqtt::connect_options_builder *> m_builder;
+    std::set<int> m_indexUsed;
+    std::mutex m_mutex;
+
+public:
+    ~ConnectOptionMgr();
+
+    int make();
+
+    mqtt::connect_options_builder *getOption(int index);
+
+    void destroy(int index);
+
+    void reset();
+
+private:
+    int makeIndex();
+};
+
+extern ConnectOptionMgr g_connectOptionMgr;
+
+#endif
