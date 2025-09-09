5743ec5ef22e6173e5f59321d26bd0454c56632e
H0meV <admin@56fkj.cn>
2025-09-09 11:38:58 +0800

    build sh

---
+++ build.sh
@@ -0,0 +1,10 @@
+TARGET_DIR="build"
+
+rm -rf $TARGET_DIR
+mkdir $TARGET_DIR && cd $TARGET_DIR
+cmake .. \
+      -DPAHO_WITH_MQTT_C=TRUE \
+      -DPAHO_BUILD_STATIC=TRUE \
+      -DPAHO_ENABLE_TESTING=FALSE
+
+make -j8
